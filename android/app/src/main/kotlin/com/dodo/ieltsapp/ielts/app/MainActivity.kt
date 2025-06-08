8034d30d6b3ecf19f4e340c21469efcebd6caea7
Duaa Ahmed <ektfaaduaa@gmail.com>
2025-06-08 13:14:48 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/dodo/ieltsapp/ielts/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dodo.ieltsapp.ielts_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:ielts_app/app/app.dart';
+import 'package:ielts_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
